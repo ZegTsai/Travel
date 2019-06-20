90222ea8feb222a09e65c3d8ac4c08262aadb471
e <846494525@qq.com>
2019-06-21 00:17:28 +0800

    change

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,18 +1,24 @@
 <template>
-    <div class="header">
+    <div class="header" id="city-header">
       <div class="header-top">
         <div class="header-top-left">
           <router-link to="/">
           <span class="iconfont home-back">&#xe624;</span>
           </router-link>
         </div>
+        <!-- 城市选择标题 -->
         <div class="header-top-right">
         <p class="city-select">城市选择</p>
         </div>
       </div>
+        <!-- 境内外选择 -->
       <div class="header-bottom">
         <div class="header-china">境内</div>
         <div class="header-board">境外·港澳台</div>
+      </div>
+      <!-- 查找框 -->
+      <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
       </div>
     </div>
 </template>
@@ -25,8 +31,10 @@ export default {
 
 <style lang="stylus" scoped>
   .header
+    position: relative
+    overflow: scroll
     width: 100%
-    height: 1.5rem
+    height: 2.2rem
     background-color: #00bcd4
     .header-top
       height: .8rem
@@ -69,4 +77,18 @@ export default {
         margin-left: 0
         float: left
         border: .005rem solid #fff
+    .search
+      background-color: #00bcd4
+      height: .7rem
+      .search-input
+        box-sizing: border-box
+        width: 80%
+        height: .5rem
+        margin-left: 9%
+        margin-top: .1rem
+        text-align: center
+        padding: 0 .1rem
+        background-color: #fff
+        border-radius: .06rem
+        color: #666
 </style>
