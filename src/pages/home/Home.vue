5f1accc61264bbabfacda1da02cd2efecd281cdd
e <846494525@qq.com>
2019-06-22 14:07:06 +0800

    finish page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,17 +14,22 @@ import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import { mapState } from 'vuex'
 import axios from 'axios'
 
 export default {
   name: 'Home',
   data () {
     return {
+      lastCity: ' ',
       swiperList: [],
       recommendList: [],
       icons: [],
       WkList: []
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   components: {
     HomeHeader,
@@ -35,7 +40,8 @@ export default {
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      /* 发送ajax请求时，应加入当前城市信息，让其加载对应城市的数据 */
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -50,7 +56,16 @@ export default {
     }
   },
   mounted () {
+    /* 保存页面最近一次设置的城市 */
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  /* activated是keepalive的生命周期函数，每次页面重新显示时会触发activated */
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
